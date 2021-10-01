cbd0e92a99adc9f26ed0bf3159a20dce884373f1
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-02 00:48:21 +0300

    add src/games/even.js

---
+++ src/games/even.js
@@ -0,0 +1,19 @@
+import getRandomNumber from '../utilities.js';
+import startNewGame from '../index.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const generateQuestionAnswer = () => {
+  const fromNumber = 0;
+
+  const question = getRandomNumber(fromNumber);
+  const answer = isEven(question) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const evenBrainGame = () => startNewGame(description, generateQuestionAnswer);
+
+export default evenBrainGame;
