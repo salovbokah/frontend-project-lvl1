b76bb5c4ac6e0c43daf1e7a77d4b04ca647c6a29
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-02 15:08:02 +0300

    add calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,49 @@
+import getRandomNumber from '../utilities.js';
+import startNewGame from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const operators = ['+', '-', '*'];
+
+const calculateExpression = (firstNumber, operator, secondNumber) => {
+  let correctAnswer;
+
+  switch (operator) {
+    case '+':
+      correctAnswer = firstNumber + secondNumber;
+      break;
+
+    case '-':
+      correctAnswer = firstNumber - secondNumber;
+      break;
+
+    case '*':
+      correctAnswer = firstNumber * secondNumber;
+      break;
+
+    default:
+      correctAnswer = null;
+  }
+
+  return correctAnswer;
+};
+
+const generateQuestionAnswer = () => {
+  const fromNumber = 0;
+
+  const firstNumber = getRandomNumber(fromNumber);
+  const secondNumber = getRandomNumber(fromNumber);
+
+  const toNumber = operators.length - 1;
+
+  const operator = operators[getRandomNumber(fromNumber, toNumber)];
+
+  const question = `${firstNumber} ${operator} ${secondNumber}`;
+  const answer = calculateExpression(firstNumber, operator, secondNumber).toString();
+
+  return [question, answer];
+};
+
+const calcBrainGames = () => startNewGame(description, generateQuestionAnswer);
+
+export default calcBrainGames;
