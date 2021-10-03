264824929cd7189351c864dbc127407f8f46fe47
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-03 18:09:42 +0300

    progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,36 @@
+import getRandomInt from '../utilities.js';
+import startNewGame from '../index.js';
+
+const getProgression = (start, step, length) => {
+  const progressions = [];
+  for (let i = 0; i < length; i++) {
+    progressions.push(start + step * i);
+  }
+  return progressions;
+};
+
+const description = 'What number is missing in the progression?';
+
+const getRandomProgressionItem = (min = 5, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateQuestionAnswer = () => {
+  const itemsCount = getRandomProgressionItem();
+
+  const progressions = getProgression(getRandomInt(), getRandomInt(), itemsCount);
+
+  const firstItem = 1;
+  const lastItem = itemsCount - 1;
+  const hiddenItem = getRandomInt(firstItem, lastItem);
+
+  const answer = progressions[hiddenItem].toString();
+
+  progressions[hiddenItem] = '..';
+
+  const question = progressions.join(' ');
+
+  return [question, answer];
+};
+
+const progressionBrainGames = () => startNewGame(description, generateQuestionAnswer);
+
+export default progressionBrainGames;
