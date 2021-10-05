e02aaff01fb7a63621592d6ffcdf116d4538ed8f
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-05 13:46:59 +0300

    add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import getRandomInt from '../utilities.js';
+import startNewGame from '../index.js';
+
+const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateQuestionAnswer = () => {
+  const question = getRandomInt();
+  const answer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const primeBrainGames = () => startNewGame(description, generateQuestionAnswer);
+
+export default primeBrainGames;
