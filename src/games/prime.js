f5416c7ba39834401a64645b7bcf257465b29081
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-05 13:57:59 +0300

    minor fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ const isPrime = (num) => {
     return false;
   }
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
