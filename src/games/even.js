acd38039724639ac064d3f440d6e231d952f3b76
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-10 19:40:08 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,12 @@
 import getRandomInt from '../utilities.js';
 import startNewGame from '../index.js';
 
-const isEven = (number) => number % 2 === 0;
-
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const generateQuestionAnswer = () => {
-  const fromNumber = 0;
+const isEven = (number) => number % 2 === 0;
 
-  const question = getRandomInt(fromNumber);
+const generateQuestionAnswer = () => {
+  const question = getRandomInt(0, 100);
   const answer = isEven(question) ? 'yes' : 'no';
 
   return [question, answer];
