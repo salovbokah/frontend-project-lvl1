30117e3f46c28a9e93655370f3c030e553487314
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-10 19:39:06 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const roundCount = 3;
+const roundsCount = 3;
 
 const startNewGame = (description, generateQuestionAnswer) => {
   console.log('Welcome to the Brain Games!');
@@ -9,15 +9,18 @@ const startNewGame = (description, generateQuestionAnswer) => {
 
   console.log(description);
 
-  for (let i = 0; i < roundCount; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, answer] = generateQuestionAnswer();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === answer) {
       console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \nLet's try again, ${name}!`);
+    }
+
+    if (userAnswer !== answer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
