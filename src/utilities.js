40d90ede0efecc4699c3d1fd8975e0b6676f6276
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-10 19:38:35 +0300

    update utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
-const getRandomInt = (min = 1, max = 99) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandomInt;
