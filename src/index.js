4557109b851c1a318a96f0fab5538e739b0b9037
Daniel Akopdjanyan <dzblik@gmail.com>
2021-10-16 13:53:10 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -14,15 +14,12 @@ const startNewGame = (description, generateQuestionAnswer) => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === answer) {
-      console.log('Correct!');
-    }
-
     if (userAnswer !== answer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${name}!`);
 };
